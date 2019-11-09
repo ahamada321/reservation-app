2e22283f886409ffe3082aadbbd9bbd4a8dc6d9e
Atsushi Hamada <a.hamada.biz@gmail.com>
2019-11-09 18:32:09 +0900

    Heroku用セットアップ

--- server/index.js
+++ server/index.js
@@ -1,24 +1,34 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const config = require('./config/dev')
+const config = require('./config')
 const SampleDb = require('./sample-db')
 
 const productRoutes = require('./routes/products')
-
+const path = require('path')
 
 mongoose.connect(config.DB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(
   () => {
-    const sampleDb = new SampleDb()
-    sampleDb.initDb()
+    if(process.env.NODE_ENV !== 'production') {
+      const sampleDb = new SampleDb()
+      // sampleDb.initDb()
+    }
   }
 )
 
 const app = express()
 
 app.use('/api/v1/products', productRoutes)
+
+if(process.env.NODE_ENV === 'production') {
+  const appPath = path.join( __dirname, '..', 'dist', 'reservation-app')
+  app.use(express.static(appPath))
+  app.get("*", function(req, res) {
+    res.sendFile(path.resolve(appPath, 'index.html'))
+  })
+}
 
 
 const PORT = process.env.PORT || '3001'
